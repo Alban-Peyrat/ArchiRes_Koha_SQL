7561c231a92f67dfb56f7c7fc0b9988551d95d9b
Alban Peyrat (Archi) <alban.peyrat@paris-est.archi.fr>
2023-04-04 16:42:13 +0200

    v2 BRDX w etudiants

--- Administration - Spécifiques/ecoles/BRDX_W_etudiants_Nouvelle_Aquitaine.sql
+++ Administration - Spécifiques/ecoles/BRDX_W_etudiants_Nouvelle_Aquitaine.sql
@@ -1,39 +1,144 @@
+/* MINIFY OBLIGATOIRE */
+
 SELECT COALESCE(CONCAT(b.title, " : ", bi.volume), b.title) AS "title",
     b.author as "author",
+    REGEXP_REPLACE(
+        REGEXP_REPLACE(
+            REGEXP_REPLACE(
+                REGEXP_REPLACE(
+                    REGEXP_REPLACE(
+                        REGEXP_REPLACE(
+                            REGEXP_REPLACE(
+                                REGEXP_REPLACE(
+                                    REGEXP_REPLACE(
+                                        REGEXP_REPLACE(
+                                            bm.metadata,
+                                            /* Replace \n by spaces */
+                                            "\n",
+                                            " "
+                                        ),
+                                        /* Clear white spaces between tags */
+                                        ">\\s*<",
+                                        "><"
+                                    ),
+                                    /* Remove all non-70X datafields*/
+                                    '<datafield tag="(?!70).*?<\/datafield>',
+                                    ""
+                                ),
+                                /* Remove all non-datafield*/
+                                '.*?(<datafield.*<\/datafield>).*',
+                                "\\1"
+                            ),
+                            /* Remove all non $a subfield*/
+                            '<subfield code="[^a|b]">.*?<\/subfield>',
+                            ""
+                        ),
+                        /* Add coma bewteen surname and First name*/
+                        "subfield><subfield",
+                        "subfield>, <subfield"
+                    ),
+                    /* Remove everything between < > */
+                    "<.*?>",
+                    "|"
+                ),
+                /* Removes multiples | */
+                "\\|+",
+                "|"
+            ),
+            /* Remove leadeing and trailing | */
+            "^\\||\\|$",
+            ""
+        ),
+        /* Remove | between surname and first name*/
+        "\\|, \\|",
+        ", "
+    ) AS "author_v2",
+    SUBSTR(ExtractValue(bm.metadata, '//datafield[@tag="100"]/subfield[@code="a"]'), 10, 4) AS "date_100",
     bi.publicationyear AS "date_raw",
     IF(REGEXP_SUBSTR(bi.publicationyear, "\\d{4}") != "", REGEXP_SUBSTR(bi.publicationyear, "\\d{4}"), bi.publicationyear) AS "date_mod",
     av.lib AS "typedoc_name",
-    CONCAT(
-        ExtractValue(bm.metadata, '//datafield[@tag="609"]/subfield[@code="a"][1]'),
-        '|_',
-        ExtractValue(bm.metadata, '//datafield[@tag="609"]/subfield[@code="a"][2]'),
-        '|_',
-        ExtractValue(bm.metadata, '//datafield[@tag="609"]/subfield[@code="a"][3]')
+    REGEXP_REPLACE(
+        REGEXP_REPLACE(
+            REGEXP_REPLACE(
+                REGEXP_REPLACE(
+                    REGEXP_REPLACE(
+                        REGEXP_REPLACE(
+                            REGEXP_REPLACE(
+                                REGEXP_REPLACE(
+                                    bm.metadata,
+                                    /* Replace \n by spaces */
+                                    "\n",
+                                    " "
+                                ),
+                                /* Clear white spaces between tags */
+                                ">\\s*<",
+                                "><"
+                            ),
+                            /* Remove all non-609 datafields*/
+                            '<datafield tag="(?!609).*?<\/datafield>',
+                            ""
+                        ),
+                        /* Remove all non-datafield*/
+                        '.*?(<datafield.*<\/datafield>).*',
+                        "\\1"
+                    ),
+                    /* Remove all non $a subfield*/
+                    '<subfield code="[^a]">.*?<\/subfield>',
+                    ""
+                ),
+                /* Remove everything between < > */
+                "<.*?>",
+                "|"
+            ),
+            /* Removes multiples | */
+            "\\|+",
+            "|"
+        ),
+        /* Remove leadeing and trailing | */
+        "^\\||\\|$",
+        ""
     ) AS "localisation",
-    ExtractValue(bm.metadata, 'count(//datafield[@tag="609"]/subfield[@code="a"])') AS "localisation_count",
-    /* ExtractValue(bm.metadata, '//datafield[@tag="610"]/subfield[@code="a"]') AS "keywords",*/
-    CONCAT(
-        ExtractValue(bm.metadata, '//datafield[@tag="610"]/subfield[@code="a"][1]'),
-        '|_',
-        ExtractValue(bm.metadata, '//datafield[@tag="610"]/subfield[@code="a"][2]'),
-        '|_',
-        ExtractValue(bm.metadata, '//datafield[@tag="610"]/subfield[@code="a"][3]'),
-        '|_',
-        ExtractValue(bm.metadata, '//datafield[@tag="610"]/subfield[@code="a"][4]'),
-        '|_',
-        ExtractValue(bm.metadata, '//datafield[@tag="610"]/subfield[@code="a"][5]'),
-        '|_',
-        ExtractValue(bm.metadata, '//datafield[@tag="610"]/subfield[@code="a"][6]'),
-        '|_',
-        ExtractValue(bm.metadata, '//datafield[@tag="610"]/subfield[@code="a"][7]'),
-        '|_',
-        ExtractValue(bm.metadata, '//datafield[@tag="610"]/subfield[@code="a"][8]'),
-        '|_',
-        ExtractValue(bm.metadata, '//datafield[@tag="610"]/subfield[@code="a"][9]'),
-        '|_',
-        ExtractValue(bm.metadata, '//datafield[@tag="610"]/subfield[@code="a"][10]')
+    REGEXP_REPLACE(
+        REGEXP_REPLACE(
+            REGEXP_REPLACE(
+                REGEXP_REPLACE(
+                    REGEXP_REPLACE(
+                        REGEXP_REPLACE(
+                            REGEXP_REPLACE(
+                                REGEXP_REPLACE(
+                                    bm.metadata,
+                                    /* Replace \n by spaces */
+                                    "\n",
+                                    " "
+                                ),
+                                /* Clear white spaces between tags */
+                                ">\\s*<",
+                                "><"
+                            ),
+                            /* Remove all non-610 datafields*/
+                            '<datafield tag="(?!610).*?<\/datafield>',
+                            ""
+                        ),
+                        /* Remove all non-datafield*/
+                        '.*?(<datafield.*<\/datafield>).*',
+                        "\\1"
+                    ),
+                    /* Remove all non $a subfield*/
+                    '<subfield code="[^a]">.*?<\/subfield>',
+                    ""
+                ),
+                /* Remove everything between < > */
+                "<.*?>",
+                "|"
+            ),
+            /* Removes multiples | */
+            "\\|+",
+            "|"
+        ),
+        /* Remove leadeing and trailing | */
+        "^\\||\\|$",
+        ""
     ) AS "keywords",
-    ExtractValue(bm.metadata, 'count(//datafield[@tag="610"]/subfield[@code="a"])') AS "keywords_count",
     ExtractValue(bm.metadata, '//datafield[@tag="330"]/subfield[@code="a"]') AS "abstract",
     CONCAT("http://www.archires.archi.fr/recherche/viewnotice/id_sigb/", biblionumber, "/id_site/*/id_profil/61") AS "archires_link",
     IF(
@@ -68,4 +173,6 @@ WHERE
 Rapport ID (prod) : 
 
 NE PAS MODIFIER - UTILISÉ PAR UN SERVICE EXTÉRIEUR
-#AR244 : liste les travaus étudiants de l'ENSAP Bordeaux ayant comme sujet la Nouvelle Aquitaine */
+#AR244 : liste les travaus étudiants de l'ENSAP Bordeaux ayant comme sujet la Nouvelle Aquitaine
+
+MINIFY OBLIGATOIRE */
