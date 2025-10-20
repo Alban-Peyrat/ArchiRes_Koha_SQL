d2242b22d94fe5ed73ee9dfe873b28fbec17b88d
Alban Peyrat (Archi) <alban.peyrat@paris-est.archi.fr>
2025-10-20 13:08:32 +0200

    List auth val for lib update

--- Administration - Spécifiques/list_auth_vals_for_library.sql
+++ Administration - Spécifiques/list_auth_vals_for_library.sql
@@ -1,14 +1,20 @@
 SELECT av.authorised_value AS "code",
-    av.lib AS "nom"
+    av.lib AS "name",
+    IF(avb.branchcode IS NULL, "All libraries", "Limited") AS "restriction"
+
 
 FROM authorised_values av
 LEFT JOIN authorised_values_branches avb ON av.id = avb.av_id
 
 WHERE av.category = <<Code valeur autorisée (LOC = localisation, coll = Collections)>>
-    AND avb.branchcode = <<Bibliothèque|branches>>
+    AND (
+        avb.branchcode = <<Bibliothèque|branches>>
+        OR avb.branchcode IS NULL
+    )
 
 GROUP BY av.id
+ORDER BY av.authorised_value ASC
 
 /* Rapport ID (prod) : 1600
 
-Liste les valeurs autorisées d'une valeur autorisé spécifiques à une bibliothèque  */
+Liste les valeurs autorisées d'une valeur autorisé spécifiques à une bibliothèque / celles sans restriction */
